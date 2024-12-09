9bf64e425e120838336671a06ec6628629ba007d
Niru <niruvk04@gmail.com>
2024-12-09 15:48:49 -0500

    Filtering for Max Time Database Fix

--- DatabaseClient.py
+++ DatabaseClient.py
@@ -391,7 +391,7 @@ if __name__ == "__main__":
   
         methods = ast.literal_eval(methods)
 
-        db.insert_recipe(row[1]["title"], row[1]["difficulty"], row[1]["vegetarian"], row[1]["vegan"], row[1]["dairy_free"], row[1]["keto"], row[1]["gluten_free"], standardized_ingredients, row[1]["picture_url"], converted_ingredients, methods, row[1]["recipe_urls"], row[1]["total_time"], row[1]["makes"], row[1]["servings"])
+        db.insert_recipe(row[1]["title"], row[1]["difficulty"], row[1]["vegetarian"], row[1]["vegan"], row[1]["dairy_free"], row[1]["keto"], row[1]["gluten_free"], standardized_ingredients, row[1]["picture_url"], converted_ingredients, methods, row[1]["recipe_urls"], int(row[1]["total_time"]), row[1]["makes"], row[1]["servings"])
     # print("Database Population Complete")
     # col = db.db["Recipes"]
     # col.create_index("title")
